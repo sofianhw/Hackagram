1d4b29ddfb0740325a610473d8843b8cd1024275
unknown <anandpranavv@gmail.com>
2015-01-16 22:06:00 -0800

    Added new activity and added it to manifest

---
+++ main/src/com/hackagram/AfterLogin.java
@@ -0,0 +1,14 @@
+package com.hackagram;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AfterLogin extends Activity {
+	
+	@Override 
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.after_login);
+	}
+
+}

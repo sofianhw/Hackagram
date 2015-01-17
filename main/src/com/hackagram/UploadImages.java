3a332fa29b67d6e04b07ac4018408b3e8d46ce54
unknown <anandpranavv@gmail.com>
2015-01-16 21:28:28 -0800

    Modified manifest

--- main/src/com/hackagram/UploadImages.java
+++ main/src/com/hackagram/UploadImages.java
@@ -1,5 +1,14 @@
 package com.hackagram;
 
-public class UploadImages {
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.MediaStore;
+
+
+
+public class UploadImages extends Activity {
+	
+	//replace ip with server address
+	public static final String FILE_UPLOAD_URL = "91912384098230980998";
 
 }
